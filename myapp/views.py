ee987c4aef9229b5da517e602b915ae49cfbc1d7
Sukanth07 <sukanthkannansk@gmail.com>
2023-05-17 11:18:14 +0530

    nk

--- myapp/views.py
+++ myapp/views.py
@@ -4,19 +4,20 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
 import cv2
-import os
+#import os
 
-current_directory = os.path.dirname(os.path.abspath(__file__))
-jewellery_img_path = os.path.join(current_directory, 'jewellery8.png')
-jewellery_img = cv2.imread(jewellery_img_path, -1)
+#current_directory = os.path.dirname(os.path.abspath(__file__))
+#jewellery_img_path = os.path.join(current_directory, 'jewellery8.png')
+#jewellery_img = cv2.imread(jewellery_img_path, -1)
 
 @csrf_exempt
 @api_view(['POST'])
 # Create your views here.
 def home(request):
 
-    user_face = request.POST.get('url')
+    user_face = request.POST.get('user_face')
+    jewellery = request.POST.get('jewellery')
     cv2.imread(user_face,-1)
-    cv2.imread(jewellery_img,-1)
+    cv2.imread(jewellery,-1)
     message = "image got successfully"
     return HttpResponse(message)
