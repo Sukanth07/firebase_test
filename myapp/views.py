722495f77e6f3cf255009f4629460a887f853e97
Sukanth07 <sukanthkannansk@gmail.com>
2023-05-17 10:46:48 +0530

    change1

--- myapp/views.py
+++ myapp/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
+import cv2
 
 @csrf_exempt
 @api_view(['POST'])
 # Create your views here.
 def home(request):
 
-    name = request.POST.get('name')
-    return HttpResponse(f"hello, {name}")
+    user_face = request.POST.get('url')
+    cv2.imread(user_face,-1)
+    message = "image got successfully"
+    return HttpResponse(message)
